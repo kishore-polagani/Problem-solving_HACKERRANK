19c1d27369958a761d10f244da0b900e00591ef9
kishore-polagani <94750532+kishore-polagani@users.noreply.github.com>
2022-10-17 22:27:52 +0530

    Create diagonal difference.py

---
+++ diagonal difference.py
@@ -0,0 +1,50 @@
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+9 8 9  
+The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+Function description
+
+Complete the  function in the editor below.
+
+diagonalDifference takes the following parameter:
+
+int arr[n][m]: an array of integers
+Return
+
+int: the absolute diagonal difference
+Input Format
+
+The first line contains a single integer, , the number of rows and columns in the square matrix .
+Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+Constraints
+
+Output Format
+
+Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+Sample Output
+
+15
+....
+    # Write your code here
+    p=0
+    k=0
+    l=0
+    for i in range(len(arr)):
+        p=p+arr[i][i]
+        k=k+arr[i][len(arr)-i-1]
+    b=abs(p-k)
+    return b
+    
